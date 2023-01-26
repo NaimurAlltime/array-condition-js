731a553f92726bd040135aed75d3a17c7d873bb6
Naimur Rahman <naimur2231@gmail.com>
2023-01-27 00:14:15 +0600

    push and pop opereation in js

---
+++ push-pop.js
@@ -0,0 +1,19 @@
+// push operation 
+var numbers = [23, 35, 45, 34, 29];
+console.log(numbers);
+
+numbers.push(37);
+console.log(numbers);
+
+
+// pop operation 
+var friendsName = ['kalam', 'jamal', 'ashik', 'karim', 'jakir'];
+console.log(friendsName);
+
+friendsName.pop();
+// friendsName.pop();
+console.log(friendsName);
+
+var element = friendsName.pop();
+console.log(element);
+console.log(friendsName);
