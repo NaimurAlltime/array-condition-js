0799c04ebc10bb2f2797ac51491388366563bd6e
Naimur Rahman <naimur2231@gmail.com>
2023-01-26 23:16:22 +0600

    array js

---
+++ array.js
@@ -0,0 +1,9 @@
+// array declaration
+var friendsName = ['naim', 'sabbir', 'kajol', 'akash'];
+var friendsAge = [21, 22, 18, 19, 24];
+var picnicFee = [200, 400, 600, 500, 800];
+
+console.log(friendsName);
+console.log(friendsAge);
+console.log(friendsAge.length);
+console.log(friendsName.length);
