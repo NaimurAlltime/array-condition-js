a4785ba9cc8f3e08fa5b9aec32da766f7ff9df78
Naimur Rahman <naimur2231@gmail.com>
2023-01-26 23:54:41 +0600

    array index in js

---
+++ index.js
@@ -0,0 +1,12 @@
+var numbers = [10, 20 , 30, 16, 48, 7, 40];
+// console.log(numbers.length);
+console.log(numbers[3]);
+
+// set value index 
+numbers[3] = 77;
+numbers[5] = 87;
+console.log(numbers);
+
+// find index number 
+var positionIndex = numbers.indexOf(20);
+console.log(positionIndex);
