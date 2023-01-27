851334385f3b7a349f7406d3f223caf87e73e2a9
Naimur Rahman <naimur2231@gmail.com>
2023-01-27 20:52:41 +0600

    compersion in js

---
+++ compersion.js
@@ -0,0 +1,15 @@
+console.log(5 > 3);
+console.log(5 < 3);
+console.log(5 == 6);
+console.log(5 != 6);
+
+var num1 = 10;
+var num2 = 20;
+
+console.log(num1 == num2);
+
+// sob gula sorto puron korte hobe 
+console.log(num1 > num2 && num1 < num2 && num1 == num2);
+
+// jekono akta sorto puron korte hobe 
+console.log(num1 > num2 || num1 < num2 || num1 == num2);
