6500b5d01a46f25aaecd19c97fe5a1e8d9144023
Naimur Rahman <naimur2231@gmail.com>
2023-01-27 21:17:42 +0600

    conditional js

---
+++ conditional.js
@@ -0,0 +1,8 @@
+var iphonePrice = 45000;
+var myBudget = 30000;
+
+if(iphonePrice <= myBudget){
+    console.log("iphone kinbo!");
+}else{
+    console.log("onno phone kinbo");
+}
---
+++ multiple-condition.js
@@ -0,0 +1,17 @@
+// sob gula sorto puron korte hobe 
+var isUnmarried = true;
+var salary = 40000;
+var car = 1;
+
+if(isUnmarried == true && salary >= 50000 && car == 2){
+    console.log("Biye Done!");
+}else{
+    console.log("Biye Cancel");
+}
+
+// jekono akta sorto puron korte hobe 
+if(isUnmarried == true || salary >= 40000 || car == 2){
+    console.log("Biye Done!");
+}else{
+    console.log("Biye Cancel");
+}
