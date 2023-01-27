64b8d829ab367b4e22897f4fabe3528d06ca825b
Naimur Rahman <naimur2231@gmail.com>
2023-01-27 22:24:51 +0600

    multiple condition find the max number

---
+++ max-number.js
@@ -0,0 +1,12 @@
+var num1 = 10;
+var num2 = 20;
+var num3 = 30;
+
+if(num1 > num2 && num1 > num3){
+    console.log("max number is num1");
+}
+else if(num2 > num1 && num2 > num3){
+    console.log("max number is num2");
+}else{
+    console.log("max number is num3");
+}
---
+++ nasted-condition.js
@@ -0,0 +1,17 @@
+var isMath = true;
+var isPhysics = true;
+var isChemistry = true;
+
+if(isMath == true){
+    if(isPhysics = true){
+        if(isChemistry !== false){
+            console.log("Computer Science And Engineering Done!!!");
+        }else{
+            console.log("Computer Engineering porte parbena");
+        }
+    }else{
+        console.log("Computer Engineering porte parbena");
+    }
+}else{
+    console.log("Computer Engineering porte parbena");
+}
