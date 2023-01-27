f8894f3cf8b0495c50629d7a4a9b6989ec20d355
Naimur Rahman <naimur2231@gmail.com>
2023-01-27 21:45:13 +0600

    multiple codition stage find in grade

---
+++ grade.js
@@ -0,0 +1,22 @@
+var grade = 83;
+
+if(grade >= 80){
+    console.log("You got A+");
+}
+else if(grade >= 70){
+    console.log("You got A");
+}
+else if(grade >= 60){
+    console.log("You got A-");
+}
+else if(grade >= 50){
+    console.log("You got B");
+}
+else if(grade >= 40){
+    console.log("You got C");
+}
+else if(grade >= 33){
+    console.log("You got D");
+}else{
+    console.log("You got F"); 
+}
